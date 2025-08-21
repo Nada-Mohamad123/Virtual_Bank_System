562e4bdf3a40724f16d6ef7d58bc5d32aada504c
Nada-Mohamad123 <nadamohamadmaklouf@gmail.com>
2025-08-21 18:36:24 +0300

    first commit

---
+++ src/main/java/com/Virtual_Bank_System/Virtual_Bank_System/VirtualBankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Virtual_Bank_System.Virtual_Bank_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBankSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBankSystemApplication.class, args);
+	}
+
+}
